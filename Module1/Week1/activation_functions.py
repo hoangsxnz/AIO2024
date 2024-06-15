6daf6c7b7215fbbe7e568c5e2fdc20e04b056c32
hoangsxnz <daylahoangson@gmail.com>
2024-06-15 20:40:59 +0700

    format code week1

--- Module1/Week1/activation_functions.py
+++ Module1/Week1/activation_functions.py
@@ -3,26 +3,30 @@ import math
 alpha = 0.01
 
 # Given
+
+
 def is_number(n):
     try:
-        float(n) # Type - casting the string to ‘float ‘.
-                  # If string is not a valid ‘float ‘ ,
-                  # it ’ll raise ‘ValueError ‘ exception
+        float(n)  # Type - casting the string to ‘float ‘.
+        # If string is not a valid ‘float ‘ ,
+        # it ’ll raise ‘ValueError ‘ exception
     except ValueError:
         return False
     return True
 
-def calc_activation_function(x , activation_function):
+
+def calc_activation_function(x, activation_function):
     assert is_number(x), "x must be a number"
 
     x = float(x)
     print(f"Input x = {x}")
-    print(f"Input activation Function (sigmoid|relu|elu): {activation_function}")
+    print(
+        f"Input activation Function (sigmoid|relu|elu): {activation_function}")
 
     if activation_function == 'relu':
         if x <= 0:
             print(f"relu: f({x}) = 0")
-        else :
+        else:
             print(f"relu: f({x}) = {x}")
     elif activation_function == 'sigmoid':
         print(f"sigmoid: f({x}) = {1 / (1 + math.exp(-x))}")
@@ -34,4 +38,5 @@ def calc_activation_function(x , activation_function):
     else:
         print(f"{activation_function} is not supported")
 
-calc_activation_function(1.5,'elu')
+
+calc_activation_function(1.5, 'elu')
--- Module1/Week1/regression_loss_functions.py
+++ Module1/Week1/regression_loss_functions.py
@@ -1,21 +1,23 @@
 import random
 import math
 
+
 def regression(num_samples, loss_name):
     assert num_samples.isnumeric(), "number of samples must be an integer number"
     assert loss_name in ['MAE', 'MSE', 'RMSE'], f"{loss_name} is not supported"
 
     num_samples = int(num_samples)
 
-    print(f"Input number of samples (interger number) which are generated: {num_samples}")
+    print(
+        f"Input number of samples (interger number) which are generated: {num_samples}")
     print(f"Input loss name: {loss_name}")
 
     sum = 0
     loss = 0
 
     for _ in range(num_samples):
-        predict = random.uniform(0,10)
-        target = random.uniform(0,10)
+        predict = random.uniform(0, 10)
+        target = random.uniform(0, 10)
         if loss_name == 'MAE':
             loss = abs(target - predict)
             sum += loss
@@ -23,8 +25,9 @@ def regression(num_samples, loss_name):
             loss = (target - predict) ** 2
             sum += loss
 
-        print(f"loss name: {loss_name}, sample: {_}, pred: {predict}, target: {target}, loss: {loss}")
-    
+        print(
+            f"loss name: {loss_name}, sample: {_}, pred: {predict}, target: {target}, loss: {loss}")
+
     if loss_name == 'MAE' or loss_name == 'MSE':
         print(f"final {loss_name}: {sum/num_samples}")
     else:
